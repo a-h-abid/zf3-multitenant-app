46282fc659c1d2daf26417fe406407a0716f2e53
Ahmedul Haque Abid <a_h_abid@hotmail.com>
2016-07-12 00:23:09 +0600

    AdminLTE sidebar nav now filterable

--- public/assets/backend/adminlte/js/adminlte-sidebar.js
+++ public/assets/backend/adminlte/js/adminlte-sidebar.js
@@ -1,28 +1,107 @@
-function adminLteSidebarActiveMenu(sidebarActiveMenu, sidebarMenu) {
-    sidebarMenu.find('.treeview').removeClass('active');
-    sidebarMenu.find('.treeview-menu').removeClass('menu-open');
+$.AdminLTE.sidebar = {
 
-    // Set Active Tree Menu
-    sidebarActiveMenu.parents('.treeview-menu').addClass('menu-open');
-    sidebarActiveMenu.parents('.treeview').addClass('active');
-}
+    sidebarAside      : $('.main-sidebar'),
+    sidebarSearch     : $('#sidebar-search'),
+    sidebarMenu       : $('.sidebar-menu'),
+    sidebarActiveMenu : $('.sidebar-menu').find('.current-menu').not('.treeview'),
+    sidebarTreeList   : $('.sidebar-menu').find('.treeview,.treeview-item'),
+    sidebarTreeMenu   : $('.sidebar-menu').find('.treeview-menu'),
 
-$(document).ready(function(){
+    init : function() {
+        this.showActiveMenuTree();
 
-    var sidebar = $('.main-sidebar'),
-        sidebarMenu = sidebar.find('.sidebar-menu'),
-        sidebarActiveMenu = sidebarMenu.find('.active').not('.treeview'),
-        sidebarSearch = $('#sidebar-search');
+        this.sidebarSearch.on('keyup', this.bindSearchBoxEvent);
+    },
 
-    adminLteSidebarActiveMenu(sidebarActiveMenu, sidebarMenu);
 
-    sidebarSearch.on('keyup', function(e){
+    showActiveMenuTree : function() {
+        this.sidebarTreeList.removeClass('active');
+        this.sidebarTreeMenu.removeClass('menu-open');
 
+        // Set Active Tree Menu
+        this.sidebarActiveMenu.parents('.treeview-menu').addClass('menu-open');
+        this.sidebarActiveMenu.parents('.treeview').addClass('active');
+    },
+
+
+    bindSearchBoxEvent : function(e) {
         var $self = $(this),
-            text = $self.val();
+            text = $.trim($self.val());
+
+        if (text == '') {
+            $.AdminLTE.sidebar.sidebarMenu.find('.hidden').removeClass('hidden');
+            return $.AdminLTE.sidebar.showActiveMenuTree();
+        } else {
+            $.AdminLTE.sidebar.sidebarMenu.find('.treeview,.treeview-menu,.treeview-item').removeAttr('style');
+            $.AdminLTE.sidebar.sidebarTreeList.addClass('active').addClass('hidden');
+            $.AdminLTE.sidebar.sidebarTreeMenu.addClass('menu-open').addClass('hidden');
+        }
+
+        $.AdminLTE.sidebar.sidebarMenu.find('.menu-name').each($.AdminLTE.sidebar.matchMenuPattern);
+    },
 
 
+    matchMenuPattern : function(i, el) {
 
-    });
+        var $el = $(el),
+            text = $.trim($.AdminLTE.sidebar.sidebarSearch.val()),
+            menuName = $el.text(),
+            menuListItem = $el.closest('li');
+
+        if ($.AdminLTE.sidebar.fuzzyMatch(menuName, text) != '') {
+            menuListItem.removeClass('hidden');
+            menuListItem.attr('data-menu-fuzzy-matched', '1');
+            menuListItem.parents('.treeview,.treeview-item,.treeview-menu').removeClass('hidden');
+        } else {
+            menuListItem.attr('data-menu-fuzzy-matched', '0');
+
+            if (menuListItem.closest('.treeview-item').attr('data-menu-fuzzy-matched') == '1') {
+                menuListItem.closest('.treeview-menu').removeClass('hidden');
+                menuListItem.removeClass('hidden');
+            } else {
+                menuListItem.addClass('hidden');
+            }
+        }
+    },
+
+
+    fuzzyMatch : function(text, search) {
+        /**
+         * Parameter text is a title, search is the user's search
+         */
+        // remove spaces, lower case the search so the search
+        // is case insensitive
+        var search = search.replace(/\ /g, '').toLowerCase();
+        var tokens = [];
+        var search_position = 0;
+
+        // Go through each character in the text
+        for (var n=0; n<text.length; n++)
+        {
+            var text_char = text[n];
+            // if we match a character in the search, highlight it
+            if(search_position < search.length &&
+              text_char.toLowerCase() == search[search_position])
+            {
+                //text_char = '<b>' + text_char + '</b>';
+                search_position += 1;
+            }
+            tokens.push(text_char);
+        }
+
+        // If are characters remaining in the search text,
+        // return an empty string to indicate no match
+        if (search_position != search.length)
+        {
+            return '';
+        }
+        return tokens.join('');
+    },
+};
+
+
+$(document).ready(function() {
+
+    $.AdminLTE.sidebar.init();
 
 });
